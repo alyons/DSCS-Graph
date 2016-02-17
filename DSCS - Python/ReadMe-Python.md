45361f673a50e631a596a055145332b31f531b42
Alexander Lyons <alexander.n.lyons@gmail.com>
2016-02-17 09:21:36 -0500

    Fixed Last Problem
    Now on to metal gear awesome

---
+++ DSCS - Python/ReadMe-Python.md
@@ -0,0 +1,22 @@
+# Digivolution Tracker
+
+## Purpose
+
+This application is designed to help the user find connections between various Digimon Lines and be able to carry their 
+Digimon to new places. This means that you would be able to create Digimon with specific skill lists and end at a 
+particular Digimon.
+
+## Challenges
+
+This is all graph theory across a undirected graph with 240 nodes which can have upwards of 8 edges. Due to my limited 
+understanding of graph theory, I know that this endeavor will be difficult and will probably lead to hours of hair 
+pulling and frustration. I do find it good practice to get into a language.
+
+## Test Cases that Seem to cause Trouble
+
+These are some cases that I ran into where it was difficult for me to get the correct answer via code, even though there 
+may exists some connection between them
+
+### Connection Found
+- RustTyranomon to Mastemon [RustTyranomon -> MetalGreymon => Ommimon => MetalGarurumon -> Zudomon -> Frigimon => Angewoman => Mastemon]
+- PrinceMamemon to Mastemon
--- DSCS - Python/digivolutionGraph.py
+++ DSCS - Python/digivolutionGraph.py
@@ -103,8 +103,11 @@ def load_digimon_hyper(file_name):
                     # print("End of evos")
                 else:
                     evo_line = lines[l]
-                    # print(name, "digivoles into", evo_line)
+                    #if name == "Angewomon":
+                    #    print(name, "digivoles into", evo_line)
                     evo_name = evo_line.split(' ')[0]
+                    if "\t" in evo_name:
+                        evo_name = evo_name.split('\t')[0]
                     if evo_name not in evos:
                         evos.append(evo_name)
         elif loading_state == LoadingState.skills_found:
@@ -134,7 +137,8 @@ def load_digimon_hyper(file_name):
             graph[name] = Digimon(name, digivolutions=evos, skills=skills, stage=stage)
 
     generate_dedigivolutions()
-    # generate_shortest_paths()
+    build_quick_paths()
+    generate_shortest_paths()
 
 
 def generate_dedigivolutions():
@@ -151,6 +155,20 @@ def print_digimon():
     for digimon in graph:
         print(graph[digimon])
         print(line)
+
+
+def print_digimon_path(path):
+    output = ""
+
+    for i in range(0, len(path)):
+        output += path[i]
+        if i < len(path) - 1:
+            if path[i + 1] in graph[path[i]].digivolutions:
+                output += " ==> "
+            elif path[i + 1] in graph[path[i]].dedigivolutions:
+                output += " --> "
+
+    print(output)
 
 
 def find_path(first, last, path=[]):
@@ -204,6 +222,100 @@ def find_all_paths(start, end, path=[]):
     return paths
 
 
+def find_shortest_path(start, end):
+    one_key = "%s => %s" % (start, end)
+    two_key = "%s => %s" % (end, start)
+
+    if one_key in shortest_paths:
+        return shortest_paths[one_key]
+
+    if two_key in shortest_paths:
+        return shortest_paths[two_key][::-1]
+
+    potential_chains = find_shortest_path_helper(start, end)
+
+    if len(potential_chains) > 0:
+        path = min(potential_chains, key=len)
+        path_key = "%s => %s" % (path[0], path[len(path) - 1])
+        shortest_paths[path_key] = path
+        return path
+    else:
+        for start_key in [key for key in shortest_paths if key.startswith("%s =>" % start)]:
+            for end_key in [key for key in shortest_paths if key.endswith("=> %s" % end)]:
+                end_of_start = start_key.split(" => ")[1]
+                start_of_end = end_key.split(" => ")[0]
+                potential_chains = potential_chains + find_shortest_path_helper(end_of_start, start_of_end)
+
+        if len(potential_chains) > 0:
+            path = min(potential_chains, key=len)
+            path_key = "%s => %s" % (path[0], path[len(path) - 1])
+            shortest_paths[path_key] = path
+            return path
+
+    return None
+
+
+def find_shortest_path_helper(start, end):
+    potential_chains = []
+    for start_key in [key for key in shortest_paths if key.startswith("%s =>" % start)]:
+        for end_key in [key for key in shortest_paths if key.endswith("=> %s" % end)]:
+            end_of_start = start_key.split(" => ")[1]
+            start_of_end = end_key.split(" => ")[0]
+            combo_key = "%s => %s" % (end_of_start, start_of_end)
+            #print("%s ?= %s: %s" % (end_of_start, start_of_end, str(end_of_start == start_of_end)))
+            if end_of_start == start_of_end:
+                potential_chains.append(shortest_paths[start_key] + shortest_paths[end_key][1::])
+            elif combo_key in shortest_paths:
+                potential_chains.append(shortest_paths[start_key] + shortest_paths[combo_key][1::] + shortest_paths[end_key][1::])
+
+    return potential_chains
+
+
+def find_shortest_path_mark_2(start, end):
+    should_retire = False
+    base_paths = []
+    child_paths = []
+
+    if start not in graph or end not in graph:
+        return None
+
+    if start == end:
+        return [start]
+
+    base_paths.append([start])
+
+    count = 0
+    while not should_retire:
+        leaf_added = False
+        for path in base_paths:
+            count += 1
+            print("\rProcessing path number: %d" % count)
+
+            leaf_added = False
+            if path[0] == start and path[len(path) - 1] == end:
+                return path
+
+            potential_leaves = graph[path[len(path) - 1]].digivolutions + graph[path[len(path) - 1]].dedigivolutions
+            for leaf in potential_leaves:
+                if leaf in graph and leaf not in path:
+                    child_paths.append(path + [leaf])
+                    leaf_added = True
+
+            if leaf_added:
+                for child_path in child_paths:
+                    if child_path[0] == start and child_path[len(path) - 1] == end:
+                        return child_path
+
+        del base_paths[::]
+        base_paths = base_paths + child_paths
+        del child_paths[::]
+
+        if not leaf_added:
+            should_retire = True
+
+    return None
+
+
 def find_all_digivolution_paths(digimon, path=[]):
     path = path + [digimon]
 
@@ -214,6 +326,8 @@ def find_all_digivolution_paths(digimon, path=[]):
         return [path]
 
     paths = []
+    if len(path) > 1:
+        paths.append(path)
     for evo in graph[digimon].digivolutions:
         extended_paths = find_all_digivolution_paths(evo, path)
         for p in extended_paths:
@@ -222,50 +336,155 @@ def find_all_digivolution_paths(digimon, path=[]):
     return paths
 
 
+def find_all_dedigivolution_paths(digimon, path=[]):
+    path = path + [digimon]
+
+    if digimon not in graph:
+        return []
+
+    if len(graph[digimon].dedigivolutions) == 0:
+        return [path]
+
+    paths = []
+    if len(path) > 1:
+        paths.append(path)
+    for evo in graph[digimon].dedigivolutions:
+        extended_paths = find_all_dedigivolution_paths(evo, path)
+        for p in extended_paths:
+            paths.append(p)
+
+    return paths
+
+
 def generate_shortest_paths():
-    print("Genreating all shortest paths")
+    print("Generating all shortest paths")
     total_start = datetime.now()
     for start in graph:
+        print("Starting", start)
+        begin_time = datetime.now()
+        created = 0
+        existed = 0
+        flipped = 0
         for end in graph:
             if start != end:
                 one_key = "%s => %s" % (start, end)
                 two_key = "%s => %s" % (end, start)
 
+                # print("Finding path for %s: " % (one_key),)
+
                 if one_key not in shortest_paths and two_key not in shortest_paths:
-                    current_start = datetime.now()
-                    print("Finding shortest path: %s => %s... " % (start, end),)
-                    paths = find_all_paths(start, end)
-                    current_end = datetime.now()
-                    if len(paths) > 0:
-                        shortest_paths[one_key] = min(paths, key=len)
-                        print("finished in: ", (current_end - current_start))
-                    else:
-                        print("No path found between %s and %s" % (start, end))
+                    shortest_paths[one_key] = find_shortest_path(start, end)
+                    shortest_paths[two_key] = shortest_paths[one_key][::-1]
+                    created += 1
                 elif two_key in shortest_paths:
-                    print("Reversing %s to use with %s" % (two_key, one_key))
+                    # print("Reversing %s to use with %s" % (two_key, one_key))
                     shortest_paths[one_key] = shortest_paths[two_key][::-1]
+                    flipped += 1
+                else:
+                    existed += 1
+
+                # print("Found path for", one_key)
+
+        end_time = datetime.now()
+        print("Finished %s in %s\nCreated: %d Flipped: %d Exists: %d" % (start, (end_time - begin_time), created, flipped, existed))
 
     total_end = datetime.now()
     print("Whole operation finished in: ", (total_end - total_start))
 
 
+def build_quick_paths():
+    for digimon in graph:
+        # Build all Digivolution Paths (That don't exist)
+        paths = find_all_digivolution_paths(digimon)
+        for path in paths:
+            one_key = "%s => %s" % (path[0], path[len(path) - 1])
+            two_key = "%s => %s" % (path[len(path) - 1], path[0])
+
+            if one_key not in shortest_paths:
+                shortest_paths[one_key] = path
+            else:
+                if len(path) < len(shortest_paths[one_key]):
+                    shortest_paths[one_key] = path
+
+            if two_key not in shortest_paths:
+                shortest_paths[two_key] = path[::-1]
+            else:
+                if len(path) < len(shortest_paths[two_key]):
+                    shortest_paths[two_key] = path[::-1]
+
+        # Build Quickswap Paths
+        if len(graph[digimon].digivolutions) > 1:
+            for evo_0 in graph[digimon].digivolutions:
+                for evo_1 in graph[digimon].digivolutions:
+                    if evo_0 != evo_1:
+                        path = [evo_0, digimon, evo_1]
+                        one_key = "%s => %s" % (path[0], path[len(path) - 1])
+                        two_key = "%s => %s" % (path[len(path) - 1], path[0])
+
+                        if one_key not in shortest_paths:
+                            shortest_paths[one_key] = path
+                        else:
+                            if len(path) < len(shortest_paths[one_key]):
+                                shortest_paths[one_key] = path
+
+                        if two_key not in shortest_paths:
+                            shortest_paths[two_key] = path[::-1]
+                        else:
+                            if len(path) < len(shortest_paths[two_key]):
+                                shortest_paths[two_key] = path[::-1]
+
+        if len(graph[digimon].dedigivolutions) > 1:
+            for evo_0 in graph[digimon].dedigivolutions:
+                for evo_1 in graph[digimon].dedigivolutions:
+                    if evo_0 != evo_1:
+                        path = [evo_0, digimon, evo_1]
+                        one_key = "%s => %s" % (path[0], path[len(path) - 1])
+                        two_key = "%s => %s" % (path[len(path) - 1], path[0])
+
+                        if one_key not in shortest_paths:
+                            shortest_paths[one_key] = path
+                        else:
+                            if len(path) < len(shortest_paths[one_key]):
+                                shortest_paths[one_key] = path
+
+                        if two_key not in shortest_paths:
+                            shortest_paths[two_key] = path[::-1]
+                        else:
+                            if len(path) < len(shortest_paths[two_key]):
+                                shortest_paths[two_key] = path[::-1]
+
+
+def is_complete_graph():
+    for digimon in graph:
+        if len(graph[digimon].digivolutions) == 0 and len(graph[digimon].dedigivolutions) == 0:
+            print("%s has no digivolutions or dedigivolutions")
+            return False
+
+
+
+    return True
+
+
 def main_app():
     while True:
-        print("What option would you like?")
+        print("What option would you like? (You can add args using ',' to break things up.)")
         print("1. Find Digimon Path")
         print("2. Find shortest Digimon path")
         print("3. Search for Digimon by name")
         print("4. Print Digimon Info")
         print("5. Print Digimon Full Digivolutions")
-        print("6. Print all Digimon")
-        print("7. Find Digimon with Skill")
+        print("6. Print Digimon Full Digivolutions")
+        print("7. Print all Digimon")
+        print("8. Find Digimon with Skill")
         print("Enter 'quit' or 'q' to close the program")
 
         choice = input("Enter a command: ")
 
-        if choice.lower() == 'quit' or choice.lower() == 'q':
+        args = choice.split(",")
+
+        if args[0].lower() == 'quit' or args[0].lower() == 'q':
             break
-        elif choice == '1':
+        elif args[0] == '1':
             first = input("Enter the starting Digimon: ")
             second = input("Enter the ending Digimon: ")
 
@@ -275,39 +494,31 @@ def main_app():
                 print("There is no path from %s to %s" % (first, second))
                 return
 
-            output = "This is the path found to the Digimon:\n"
-
-            for i in range(0, len(path)):
-                output += path[i]
-                if i < len(path) - 1:
-                    if path[i + 1] in graph[path[i]].digivolutions:
-                        output += " ==> "
-                    elif path[i + 1] in graph[path[i]].dedigivolutions:
-                        output += " --> "
-
-            print(output)
-        elif choice == '2':
-            first = input("Enter the starting Digimon: ")
-            second = input("Enter the ending Digimon: ")
-
-            paths = find_all_paths(first, second)
-            if len(paths) > 0:
-                path = min(paths, key=len)
-                output = "This is the path found to the Digimon:\n"
-
-                for i in range(0, len(path)):
-                    output += path[i]
-                    if i < len(path) - 1:
-                        if path[i + 1] in graph[path[i]].digivolutions:
-                            output += " ==> "
-                        elif path[i + 1] in graph[path[i]].dedigivolutions:
-                            output += " --> "
-
-                output += ""
-
-                print(output)
+            print("This is the path found to the Digimon:")
+            print_digimon_path(path)
+        elif args[0] == '2':
+            if len(args) == 3:
+                start = args[1]
+                end = args[2]
             else:
-                print("There is no path from %s to %s" % (first, second))
+                start = input("Enter the starting Digimon: ")
+                end = input("Enter the ending Digimon: ")
+
+            if start not in graph:
+                print("%s is not a Digimon that I have on record..." % start)
+            elif end not in graph:
+                print("%s is not a Digimon that I have on record..." % end)
+            else:
+                search_start = datetime.now()
+                print("Search begins at:", search_start, "...",)
+                path = find_shortest_path(start, end)
+                search_finish = datetime.now()
+                print("completed in:", search_finish - search_start)
+                if path is None:
+                    print("There is no path from %s to %s" % (start, end))
+                else:
+                    print("This is the path found to the Digimon:")
+                    print_digimon_path(path)
         elif choice == '3':
             search_term = input("Enter the search term: ")
             results = [d for d in graph.keys() if search_term in d]
@@ -331,12 +542,24 @@ def main_app():
             if name in graph:
                 paths = find_all_digivolution_paths(name)
                 for path in paths:
-                    print(path)
+                    print_digimon_path(path)
+
+                print("Total Paths: %d" % len(paths))
             else:
                 print("Please retry with a valid Digimon in this game.")
         elif choice == '6':
-            print_digimon()
+            name = input("Enter a Digimon: ")
+            if name in graph:
+                paths = find_all_dedigivolution_paths(name)
+                for path in paths:
+                    print_digimon_path(path)
+
+                print("Total Paths: %d" % len(paths))
+            else:
+                print("Please retry with a valid Digimon in this game.")
         elif choice == '7':
+            print_digimon()
+        elif choice == '8':
             name = input("Enter a skill name: ")
             results = [d for d in graph if name in graph[d].skills]
             print(results)
